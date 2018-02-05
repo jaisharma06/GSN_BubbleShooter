bc2c87b523efc733ec7f80318d735d428d4c92ea
JaiSharma614 <Jai@darwinlabs.io>
2018-02-05 16:45:50 +0530

    init commit

---
+++ Assets/Scripts/Utils/BubbleMatrixControllerHelper.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+namespace BubbleShooter
+{
+
+    public class BubbleMatrixControllerHelper
+    {
+
+        public static Vector2 CellForPosition(Vector3 position, BubbleMatrixGeometry geometry, bool isBaselineAlignedLeft)
+        {
+            int row = geometry.rows - Mathf.FloorToInt(position.y) - 1;
+            int column;
+            bool rowIsEven = row % 2 == 0;
+            if ((rowIsEven && isBaselineAlignedLeft) || (!rowIsEven && !isBaselineAlignedLeft))
+            {
+                column = Mathf.FloorToInt(position.x);
+            }
+            else
+            {
+                column = Mathf.FloorToInt(position.x - geometry.bubbleRadius);
+            }
+            Vector2 result = new Vector2(row, Utils.TruncateToInterval(column, 0, geometry.columns - 1));
+            return result;
+        }
+
+
+        public static Vector3 PositionForCell(Vector2 cell, BubbleMatrixGeometry geometry, bool isBaselineAlignedLeft)
+        {
+            bool rowIsEven = cell.x % 2 == 0;
+            float y = geometry.rows - cell.x - geometry.bubbleRadius;
+            float x;
+            if (isBaselineAlignedLeft)
+            {
+                if (rowIsEven)
+                {
+                    x = cell.y + geometry.bubbleRadius;
+                }
+                else
+                {
+                    x = cell.y + 2 * geometry.bubbleRadius;
+                }
+            }
+            else
+            {
+                if (rowIsEven)
+                {
+                    x = cell.y + 2 * geometry.bubbleRadius;
+                }
+                else
+                {
+                    x = cell.y + geometry.bubbleRadius;
+                }
+            }
+            return new Vector3(x, y, geometry.depth);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Utils/Utils.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BubbleShooter{
+	
+	public class Utils {
+	
+		public static T GetRandomEnum<T>()
+			{	
+	    		System.Array A = System.Enum.GetValues(typeof(T));
+	    		T V = (T)A.GetValue(Random.Range(0,A.Length));
+	    		return V;
+			}	
+		
+		public static Color BubbleColorToColor(BubbleColor color){
+		  switch (color){
+			case BubbleColor.Black:
+				return new Color(0.3f, 0.3f, 0.3f);
+			case BubbleColor.Blue:
+				return new Color(0.0f, 0.0f, 1.0f);
+			case BubbleColor.Green:
+				return new Color(0.0f, 1.0f, 0.0f);
+			case BubbleColor.Red:
+				return new Color(1.0f, 0.0f, 0.0f);
+			case BubbleColor.White:
+				return new Color(1.0f, 1.0f, 1.0f);
+			case BubbleColor.Yellow:
+				return new Color(1.0f, 1.0f, 0.0f);
+			default:
+				return new Color(0.0f, 1.0f, 1.0f);
+			}
+		}
+
+        public static ArrayList FilterByColor(ArrayList bubbles, BubbleColor color)
+        {
+            ArrayList filtered = new ArrayList();
+            foreach (Bubble bubble in bubbles)
+            {
+                if (bubble.color == color)
+                    filtered.Add(bubble);
+            }
+            return filtered;
+        }
+
+        public static int TruncateToInterval(int number, int min, int max)
+        {
+            int outcome;
+            outcome = number;
+            if (number < min) outcome = min;
+            if (number > max) outcome = max;
+            return outcome;
+        }
+    }
+}
